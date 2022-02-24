5577dd759f5531c3d70e3039925ced5cfda63264
Rachit Kumar <45527837+rachit-k@users.noreply.github.com>
2022-02-25 02:28:19 +0530

    Update Skill.java

--- Skill.java
+++ Skill.java
@@ -1,4 +1,13 @@
+import java.util.Comparator;
+
 public class Skill {
     String skillName;
     int skillLevel;
+}
+
+class SortSkills implements Comparator<Skill>{
+    @Override
+    public int compare(Skill a, Skill b){
+        return a.skillLevel - b.skillLevel;
+    }
 }
