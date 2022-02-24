e6bb9cea468a57404ca902223745b27fbe4b8b56
Mohit Anand <anand.mohit1499@gmail.com>
2022-02-25 00:41:43 +0530

    Add comparator for Project

--- Project.java
+++ Project.java
@@ -1,9 +1,29 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class Project {
     String name;
     int score;
-    int len;
+    int days;
     int bestBefore;
     List<Skill> skills;
+}
+
+class SortProjects implements Comparator<Project>{
+    @Override
+    public int compare (Project a, Project b){
+        int aSkillSum = 0;
+        for(int i = 0; i< a.skills.size(); i++){
+            aSkillSum =  aSkillSum + a.skills.get(i).skillLevel;
+        }
+
+        int bSkillSum = 0;
+        for(int i = 0; i< b.skills.size(); i++){
+            bSkillSum =  bSkillSum + b.skills.get(i).skillLevel;
+        }
+
+        double aScore = (Math.pow(a.score,2))/(a.days * aSkillSum);
+        double bScore = (Math.pow(b.score,2))/(b.days * bSkillSum);
+        return  (int)bScore - (int)aScore;
+    }
 }
