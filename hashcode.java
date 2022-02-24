81d9ea5be99903f50440ad26cedce46e3b06e0da
Rachit Kumar <45527837+rachit-k@users.noreply.github.com>
2022-02-25 02:27:36 +0530

    Update hashcode.java

--- hashcode.java
+++ hashcode.java
@@ -1,6 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+class Pair<T, U> {
+
+    private final T first;
+    private final U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() { return first; }
+    public U getSecond() { return second; }
+}
+
 public class hashcode{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -44,5 +58,46 @@ public class hashcode{
             obj.skills = skills;
             projects.add(obj);
         }
+
+        int currTime=0;
+
+        List<Solution> ans=new ArrayList<>();
+
+        Collections.sort(projects, new SortProjects());
+        for(int i=0;i<projects.size(); i++){
+            Solution curAns=new Solution();
+            Project curP=projects.get(i);
+            if(currTime+curP.days >= curP.bestBefore+ curP.score){
+                continue;
+            }
+            List<Skill> skills=curP.skills;
+            Collections.sort(skills, new SortSkills());
+            Map<String, Pair<Integer,Integer> > ogSkills = new HashMap<>();
+            for(int j=0; j<contributors.size(); j++){
+                Person contributor = contributors.get(j);
+                for(int k=0; k<contributor.skills.size(); k++){
+                    Skill curS=contributor.skills.get(k);
+                    Pair<Integer,Integer> pair=new Pair(curS.skillLevel, k);
+                    if(contributor.unavailableTill < currTime)
+                        ogSkills.put(curS.skillName, pair);
+                }
+            }
+            curAns.ProjectName=curP.name;
+
+            for(int j=0;j<skills.size(); j++){
+                Skill curS=skills.get(j);
+                if(ogSkills.containsKey(curS.skillName) && curS.skillLevel >= ogSkills.get(curS.skillName).getFirst()){
+                    // select this person
+                    Person contributor=contributors.get(ogSkills.get(curS.skillName).getSecond());
+                    contributor.unavailableTill=currTime+curP.days;
+                    curAns.Persons.add(contributor.name);
+                }
+            }
+
+            ans.add(curAns);
+        }
+
     }
+
+
 }
